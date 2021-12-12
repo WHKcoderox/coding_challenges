bb46e1a735974445c8b361f51fbb8285f1dfba73
hw2720 <hw2720@ic.ac.uk>
2021-12-12 01:06:01 +0000

    done editing 878

--- leetcode/878_nth_magical_number.cpp
+++ leetcode/878_nth_magical_number.cpp
@@ -11,7 +11,38 @@ typedef pair<int, int> ii;
 
 class Solution {
 public:
+    int gcd(int a, int b) {
+        if (a == b) return a;
+        if (a > b) swap(a, b);
+        return gcd(a, b-a);
+    }
+    int lcm(int a, int b) {
+        return a * b / gcd(a, b);
+    }
     int nthMagicalNumber(int n, int a, int b) {
+        if (a > b) swap(a, b);
+        // speedup with lcm
+        ll temp = lcm(a, b);
+        int ma = temp / a, mb = temp / b;
+        temp = n / (ma + mb - 1);
+        n -= temp * (ma + mb - 1);
+        temp = (ll) a * (ll) ma * temp;
+        ll n1, n2, curr;
+        n1 = n2 = curr = temp;
+        n1 += a; n2 += b;
+        while (n-- > 0) {
+            if (n1 > n2) {
+                curr = n2;
+                n2 += b;
+            } else {
+                curr = n1;
+                n1 += a;
+            }
+        }
+        return (int) (curr % INF);
+    }
+
+    int nthMagicalNumber2(int n, int a, int b) {
         // find lcm and s p e e d
         ll curr, n1, n2;
         int i = 1, j = 1, mi = INF, mj = INF, temp;
@@ -48,7 +79,7 @@ public:
 
 int main() {
     Solution t;
-    cout << t.nthMagicalNumber(5, 2, 4) << endl;
+    cout << t.nthMagicalNumber(8, 8, 8) << endl;
     cout << "done" << endl;
     return 0;
 }
